9c5624d0280fdd88250e1e3c40cdc2e48d8870df
Gabriel Santos <iel.or.santoss@gmail.com>
2024-03-27 14:36:20 -0300

    Estrutura da Header

--- src/Components/Header/Index.tsx
+++ src/Components/Header/Index.tsx
@@ -1,10 +1,26 @@
-import "./Style.css"
-
+import "./style.css"
+import salesLogo from "../../assets/Salesforce.com_logo.png"
 const Header = () => {
-    return (
+   return (
+      <header>
+         <div className="salesHeader">
+            <div className="right">
+               <img width={100} height={82} src={salesLogo} />
+            </div>
 
- <h1>ola</h1>
- 
- )
- }
-    export default Header;
+            <div className="center">
+               <h1 id="nome">Salesforce</h1>
+            </div>
+
+            <nav>
+               <button>Home</button>
+               <button>Login</button>
+               <button>Sobre</button>
+               <button>Contato</button>
+            </nav>
+         </div>
+      </header>
+   )
+}
+
+export default Header;
