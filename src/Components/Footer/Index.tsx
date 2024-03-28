26883f5291c16b111dd7e733f8a4306b87e64b77
Murilo Carvalho <papelfolinha@gmail.com>
2024-03-28 08:55:46 -0300

    Ajustes no botoes da header e criação do footer

--- src/Components/Footer/Index.tsx
+++ src/Components/Footer/Index.tsx
@@ -1,8 +1,21 @@
-import "./Style.css"
+import "./style.css"
 
 const Footer = () => {
     return (
-        <h1>ola</h1>
+        <footer>
+            <ul>
+            <li><h1>Desenvolvido por: </h1></li>
+            <li>Gabriel Oliveira RM: 553079</li>
+            <li>Lucas Martinez RM: 553816</li>
+            <li>Murillo Carvalho RM: 550551</li>
+            </ul>
+        </footer>
+
     )
 }
+
 export default Footer;
+
+
+
+
