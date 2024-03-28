c8900a2f5f65562f8f9b7797f3b40a04b941f6ab
Murilo Carvalho <papelfolinha@gmail.com>
2024-03-28 09:12:03 -0300

    Mudança na estrutura do footer e estilização

--- src/Components/Footer/Index.tsx
+++ src/Components/Footer/Index.tsx
@@ -1,14 +1,18 @@
-import "./style.css"
+import "./Style.css"
 
 const Footer = () => {
     return (
         <footer>
-            <ul>
-            <li><h1>Desenvolvido por: </h1></li>
-            <li>Gabriel Oliveira RM: 553079</li>
-            <li>Lucas Martinez RM: 553816</li>
-            <li>Murillo Carvalho RM: 550551</li>
-            </ul>
+            <h1>Desenvolvido por: </h1>
+            <div className="person">
+                <p>Gabriel Oliveira <br />RM: 553079</p>
+            </div>
+            <div className="person">
+                <p>Lucas Martinez <br />RM: 553816</p>
+            </div>
+            <div className="person">
+                <p>Murillo Carvalho <br />RM: 550551</p>
+            </div>
         </footer>
 
     )
